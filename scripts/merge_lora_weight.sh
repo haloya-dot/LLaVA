c406efd14bc503e4f3a7e2152557b3f8eba96ddc
llhuang <lilysnowlan@gmail.com>
2024-10-20 16:31:37 +0800

    merge_sh

---
+++ scripts/merge_lora_weight.sh
@@ -0,0 +1,4 @@
+python /root/autodl-tmp/LLaVA/scripts/merge_lora_weights.py \
+    --model-path "/root/autodl-tmp/LLaVA/checkpoints/llava-v1.5-7b-lora" \
+    --model-base "root/autodl-tmp/LLaVA/checkpoints/llava-v1.5-7b" \
+    --save-model-path "/root/autodl-tmp/LLaVA/checkpoints/llava-v1.5-7b-merged"
